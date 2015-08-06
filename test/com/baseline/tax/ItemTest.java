5221d5a0a3c8dd64138bd65c62c4fda216976fe5
varshapalat <varshapalat@gmail.com>
2015-08-06 10:40:18 +0530

    #0003| Added functionality to compute basic sales tax on taxable items

--- test/com/baseline/tax/ItemTest.java
+++ test/com/baseline/tax/ItemTest.java
@@ -6,9 +6,16 @@ import static org.junit.Assert.assertEquals;
 
 public class ItemTest {
     @Test
-    public void foo() {
+    public void computeSalesTaxWhenNonTaxableItem() {
         Item itemOne = new Item("Book", "Unimported", 12.49);
 
         assertEquals(0, itemOne.computeBasicSalesTax(), 0);
+    }
+
+    @Test
+    public void computeSalesTaxWhenTaxableItem() {
+        Item itemTwo = new Item("Music", "Unimported", 14.99);
+
+        assertEquals(.15, itemTwo.computeBasicSalesTax(), 0.05);
     }
 }
